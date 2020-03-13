66d5e8e92cd04ee51ce5f494c946b947a35f6e45
wj <398636806@qq.com>
2020-03-13 22:04:02 +0800

    .

---
+++ src/com/windj0y/hw02/bean/Student.java
@@ -0,0 +1,23 @@
+package com.windj0y.hw02.bean;
+
+public class Student {
+    int sid;
+    String name;
+
+    public int getSid(){
+        return sid;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+
+}
---
+++ src/com/windj0y/hw02/servlet/MainServlet.java
@@ -0,0 +1,67 @@
+package com.windj0y.hw02.servlet;
+
+import com.windj0y.hw02.jdbc.MainBo;
+import com.windj0y.hw02.jdbc.MysqlAdapter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/v1/*")
+public class MainServlet extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        MysqlAdapter.Connect();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String path = req.getPathInfo();
+        resp.setCharacterEncoding("utf-8");
+        resp.setContentType("text/html;charset=utf-8;");
+        switch (path){
+            case "/addStudent":{
+                if(MainBo.addStudent(req.getParameter("name"))){
+                    resp.getWriter().println("添加成功");
+                }else{
+                    resp.getWriter().println("添加失败");
+                }
+                break;
+            }
+            case "/addHomework":{
+                if(MainBo.addHomework(req.getParameter("name"))){
+                    resp.getWriter().println("添加成功");
+                }else{
+                    resp.getWriter().println("添加失败");
+                }
+                break;
+            }
+            case "/submitHomework":{
+                if(MainBo.submitHomework(req.getParameter("sid"),req.getParameter("hid"),req.getParameter("content"))){
+                    resp.getWriter().println("提交成功");
+                }else{
+                    resp.getWriter().println("提交失败");
+                }
+                break;
+            }
+            default:{
+                resp.getWriter().println("err");
+            }
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+        MysqlAdapter.Disconnect();
+    }
+}
